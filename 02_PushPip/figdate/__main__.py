49aa9285f09d320ba618c295702027b1a87d6ec8
Kozlov Alexandr <as.kozlov@list.ru>
2022-02-20 18:20:03 +0500

    feat 02_PushPip: simplify __main__.py

--- 02_PushPip/figdate/__main__.py
+++ 02_PushPip/figdate/__main__.py
@@ -3,10 +3,4 @@ import sys
 from .date import date
 
 locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
-argc = len(sys.argv)
-if argc <= 1:
-    print(date()) 
-elif argc == 2:
-    print(date(sys.argv[1]))
-else:
-    print(date(sys.argv[1], sys.argv[2]))
+print(date(*sys.argv[1:3]))
